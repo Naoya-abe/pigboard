73f65e26b1dc23c3df4849389e433a4dd544a64c
阿部直也 <n.abe@gemcook.com>
2019-10-19 11:55:41 +0900

    user取得削除

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,7 @@ import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
 
 class App extends React.Component {
-  state = {users: [], posts: []};
-
-  getUsers = async () => {
-    try {
-      const usersResponse = await axios.get(
-        'https://jsonplaceholder.typicode.com/users'
-      );
-      this.setState({users: usersResponse.data});
-      console.log(this.state.users);
-    } catch (error) {
-      window.alert('ユーザの取得に失敗しました。');
-    }
-  };
+  state = {posts: []};
 
   getPosts = async () => {
     try {
@@ -33,7 +21,6 @@ class App extends React.Component {
   };
 
   componentDidMount() {
-    this.getUsers();
     this.getPosts();
   }
 
@@ -45,24 +32,6 @@ class App extends React.Component {
             author="とんぺい"
             date="10月8日 4:00PM"
             description="今日の昼ごはんは美味しかったブゥ！"
-            avator={faker.image.avatar()}
-          />
-        </ApprovalCard>
-
-        <ApprovalCard>
-          <CommentDetail
-            author="師匠"
-            date="10月3日 9:00AM"
-            description="今日は午後から会議に参加しなければ。"
-            avator={faker.image.avatar()}
-          />
-        </ApprovalCard>
-
-        <ApprovalCard>
-          <CommentDetail
-            author="司書さん"
-            date="9月27日 12:00PM"
-            description="いい本が見つかったから、今度とんぺいちゃんに紹介してあげなきゃ。"
             avator={faker.image.avatar()}
           />
         </ApprovalCard>
